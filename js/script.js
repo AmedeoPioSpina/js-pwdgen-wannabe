8ceec78bdab5089731bc7ce172f8d85e70b837b6
AmedeoPioSpina <spina.amedeo.pio@gmail.com>
2023-11-09 16:00:59 +0100

    Commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let userName = prompt("Inserie nome: ");
+let userSurname = prompt("Inserie cognome: ");
+let preferiteColor = prompt("Inserie colore preferito: ");
+let randomNumber = parseInt(Math.random() * 100);
+let generatedPasswordTarget = document.getElementById("generated-password");
+
+let generatedPassword = userName + userSurname + preferiteColor + randomNumber;
+
+generatedPasswordTarget.innerHTML = `La password generata è: ${generatedPassword}`
